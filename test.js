e5d0d0bc1f974b5b288108c45845a23184f72f8a
Maeda Naoki <maeda.naoki.md9@is.naist.jp>
2016-11-18 03:22:55 +0900

    Read XAxis value

--- test.js
+++ test.js
@@ -1,8 +1,35 @@
-var SensingMouse = require("./index");
+var async = require("async");
+var sensingMouse = require("./index");
 
-function onDiscover(SensingMouse){
-	console.log("Name = " + SensingMouse.peripheral.advertisement.localName);
-	console.log("UUIDs = " + SensingMouse.peripheral.advertisement.serviceUuids);
-}
+sensingMouse.discover((sensingMouse) => {
+	console.log("Discover : " + sensingMouse);
 
-SensingMouse.discoverAll(onDiscover);
+	sensingMouse.once("disconnect" , () => {
+		console.log("Disconnect");
+		process.exit(1);
+	});
+
+	async.series([
+		function(callback){
+			sensingMouse.connectAndSetUp(callback);
+		} ,
+		function(callback){
+			sensingMouse.readDeviceName((error , deviceName) => {
+				console.log("Name = " + deviceName);
+				callback();
+			});
+		} ,
+		function(callback){
+			sensingMouse.readXAxisValue(function(error , x){
+				console.log("x = " , x);
+				callback();
+			});
+		} ,
+		function(callback){
+			sensingMouse.on("XAxisValueChange" , function(x){
+				console.log("Change		X = " , x);
+				callback();
+			});
+		}
+	]);
+});
