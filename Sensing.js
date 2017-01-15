61d0ddb2b0cc74ed3ec00c66d87120b9dad98041
Maeda Naoki <maeda.naoki.md9@is.naist.jp>
2017-01-16 04:51:55 +0900

    fix the problem of saving the same data in all files

--- Sensing.js
+++ Sensing.js
@@ -1,160 +1,127 @@
 var async = require("async");
 var sensingMouse = require("./index");
-var csv = require('csv');
-var csvWriter = require('csv-write-stream');
+var csv = require("csv")
 var fs = require('fs');
 var format = require('date-format');
-var writer = csvWriter({
-    sendHeaders: false,
-    separator: ';'
-});
 
+const stringifierAcc = csv.stringify();
+const accelerationWS = fs.createWriteStream("Acceleration.csv", {encoding: "utf-8"});
+stringifierAcc.pipe(accelerationWS);
+
+const stringifierAM2321 = csv.stringify();
+const AM2321WS = fs.createWriteStream("TemperatureHumidity.csv", {encoding: "utf-8"});
+stringifierAM2321.pipe(AM2321WS);
+
+const stringifierClick = csv.stringify();
+const clickWS = fs.createWriteStream("ClickForce.csv", {encoding: "utf-8"});
+stringifierClick.pipe(clickWS);
+
+const stringifierGrip = csv.stringify();
+const gripWS = fs.createWriteStream("GripForce.csv", {encoding: "utf-8"});
+stringifierGrip.pipe(gripWS);
+
+const stringifierGSR = csv.stringify();
+const GSRWS = fs.createWriteStream("GSR.csv", {encoding: "utf-8"});
+stringifierGSR.pipe(GSRWS);
+
+const stringifierHR = csv.stringify();
+const HRWS = fs.createWriteStream("HeartRate.csv", {encoding: "utf-8"});
+stringifierHR.pipe(HRWS);
 
 sensingMouse.discover((sensingMouse) => {
-    console.log("Discover : " + sensingMouse);
+	console.log("Discover : " + sensingMouse);
 
-    sensingMouse.once("disconnect",
-        () => {
-            console.log("Disconnect");
+	sensingMouse.once("disconnect",
+		() => {
+			console.log("Disconnect");
 
-            process.exit(0);
-        });
+			process.exit(0);
+		});
 
-    writer.pipe(fs.createWriteStream('Acc.csv', { flags: 'a' }));
-    sensingMouse.on("AccelerationValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
+	sensingMouse.on("AccelerationValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierAcc.write(values);
+		});
 
-        });
+	sensingMouse.on("AM2321ValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierAM2321.write(values);
+		});
 
-    writer.pipe(fs.createWriteStream('TempHum.csv', { flags: 'a' }));
-    sensingMouse.on("AM2321ValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
-        });
+	sensingMouse.on("HeartRateValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierHR.write(values);
+		});
 
-    writer.pipe(fs.createWriteStream('HR.csv', { flags: 'a' }));
-    sensingMouse.on("HeartRateValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
-        });
+	sensingMouse.on("GripForceValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierGrip.write(values);
+		});
 
-    writer.pipe(fs.createWriteStream('GripForce.csv', { flags: 'a' }));
-    sensingMouse.on("GripForceValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
-        });
+	sensingMouse.on("ClickForceValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierClick.write(values);
+		});
 
-    writer.pipe(fs.createWriteStream('ClickForce.csv', { flags: 'a' }));
-    sensingMouse.on("ClickForceValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
-        });
+	sensingMouse.on("GSRValueChange",
+		(values) => {
+			let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
+			values["ts"] = timeStamp;
+			stringifierGSR.write(values);
+		});
 
-    writer.pipe(fs.createWriteStream('GSR.csv', { flags: 'a' }));
-    sensingMouse.on("GSRValueChange",
-        (values) => {
-            console.log(values);
-            let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-            values["ts"] = timeStamp;
-            writer.write(values);
-        });
+	async.series([
+		function(callback) {
+			sensingMouse.connectAndSetUp(callback);
+		},
+		function(callback) {
+			sensingMouse.notifyAcceleration((error) => {
+				console.log("notify Acceleration" + error);
+			});
+			callback();
+		},
+		function(callback) {
+			sensingMouse.notifyAM2321((error) => {
+				console.log("notify AM2321" + error);
+			});
+			callback();
+		},
 
+		function(callback) {
+			sensingMouse.notifyHeartRate((error) => {
+				console.log("Notify HeartRate");
+			});
+			callback();
+		},
 
-   //writeValues = function (values, fileName) {
-   //    let timeStamp = format('yyyy:MM:dd:hh:mm:ss.SSS', new Date());
-   //    var writer = csvWriter({
-   //         sendHeaders: false,
-   //         separator: ';'
-   //    });
-   //     this.fileName = fileName;
-   //     writer.pipe(fs.createWriteStream(fileName, { flags: 'a' }));   
-   //     values["ts"] = timeStamp;
-   //     writer.write(values);
-   // };
+		function(callback) {
+			sensingMouse.notifyClickForce((error) =>  {
+			console.log("Notify ClickForce");
+			});
+			callback();
+		},
+		function(callback) {
+			sensingMouse.notifyGripForce((error) => {
+				console.log("Notify GripForce");
+			});
+			callback();
+		},
 
-
-    async.series([
-        function(callback) {
-            sensingMouse.connectAndSetUp(callback);
-        },
-        function(callback) {
-            sensingMouse.notifyAcceleration((error) => {
-                console.log("notify Acceleration" + error);
-            });
-            callback();
-        },
-        function(callback) {
-            sensingMouse.notifyAM2321((error) => {
-                console.log("notify AM2321" + error);
-            });
-            callback();
-        },
-
-        function(callback) {
-            sensingMouse.notifyHeartRate((error) => {
-                console.log("Notify HeartRate");
-            });
-            callback();
-        },
-
-        function(callback) {
-            sensingMouse.notifyClickForce((error) =>  {
-            console.log("Notify ClickForce");
-            });
-            callback();
-        },
-        function(callback) {
-            sensingMouse.notifyGripForce((error) => {
-                console.log("Notify GripForce");
-            });
-            callback();
-        },
-
-        function(callback) {
-            sensingMouse.notifyGSR((error) => {
-                console.log("Notify GSR");
-            });
-            callback();
-        }
+		function(callback) {
+			sensingMouse.notifyGSR((error) => {
+				console.log("Notify GSR");
+			});
+			callback();
+		}
 	]);
 });
-
-
-
-
-
-
-
-
-    //sensingMouse.on("ClickForceValueChange",
-    //    (values) => {
-    //        csvFile.pipe(fs.createWriteStream('ClickForce.csv'));
-    //        writer.write(values);
-    //        writer.end();
-    //        console.log("ClickRight" + values["ClickForceRight"]);
-    //        console.log("Clickleft" + values["ClickForceLeft"]);
-    //    });
-
-   //sensingMouse.on("HeartRateValueChange",
-    //    (values) => {
-    //        console.log(values);
-    //        var fileName = 'HR.csv';
-    //        writeValues(values, fileName);
-
-    //    });
